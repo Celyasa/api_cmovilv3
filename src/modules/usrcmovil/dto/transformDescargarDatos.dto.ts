3cb061388d9c11a2b50ec24aac6a195795f6d4b6
Celyasa <analistaprogramador.@celyasa.com>
2023-07-05 10:38:22 -0500

    Cambios para descargar datos para el telefono

---
+++ src/modules/usrcmovil/dto/transformDescargarDatos.dto.ts
@@ -0,0 +1,654 @@
+import { Expose, Type } from 'class-transformer';
+
+export interface DescargarData {}
+
+export class transformDescargarDatosDto {
+  @Expose({ name: 'cmovilVersion' })
+  @Type(() => String)
+  cmovilVersion: string;
+
+  @Type(() => Lq)
+  lq: Lq[];
+
+  @Type(() => Cliente)
+  cliente: Cliente[];
+
+  @Type(() => ListaPrecio)
+  listaPrecio: ListaPrecio[];
+
+  @Type(() => ProductoCategoria)
+  productoCategoria: ProductoCategoria[];
+
+  @Type(() => Producto)
+  producto: Producto[];
+
+  @Type(() => ProductoCombo)
+  productoCombo: ProductoCombo[];
+
+  @Type(() => ListaDescuento)
+  listaDescuento: ListaDescuento[];
+
+  @Type(() => TipoVisita)
+  tipoVisita: TipoVisita[];
+
+  @Type(() => ListaNegraTelefono)
+  listaNegraTelefono: ListaNegraTelefono[];
+
+  @Type(() => Presupuesto)
+  presupuesto: Presupuesto[];
+
+  @Type(() => PedidoSugerido)
+  pedidoSugerido: PedidoSugerido[];
+
+  @Type(() => AutualizacionDatosCliente)
+  autualizacionDatosCliente: AutualizacionDatosCliente[];
+
+  @Type(() => DatosCreaCliente)
+  datosCreaCliente: DatosCreaCliente[];
+
+  @Type(() => InfoCreaCliente)
+  infoCreaCliente: InfoCreaCliente[];
+
+  @Type(() => ComboMix)
+  comboMix: ComboMix[];
+}
+
+class Lq {
+  @Expose({ name: 'UCM_MODULO' })
+  @Type(() => Number)
+  ucmModulo: number;
+
+  @Expose({ name: 'RUT_ID' })
+  @Type(() => String)
+  rutId: string;
+
+  @Expose({ name: 'FECHA_LIQUIDACION' })
+  @Type(() => String)
+  fechaLiquidacion: string;
+
+  @Expose({ name: 'LIQUIDACION' })
+  @Type(() => String)
+  liquidacion: string;
+
+  @Expose({ name: 'LIQUIDACION_DATE' })
+  @Type(() => String)
+  liquidacionDate: string;
+
+  @Expose({ name: 'CCO_FECHA' })
+  @Type(() => Date)
+  ccoFecha: Date;
+
+  @Expose({ name: 'CCO_FECHAFIN' })
+  @Type(() => Date)
+  ccoFechaFin: Date;
+
+  @Expose({ name: 'CCO_CODIGO' })
+  @Type(() => String)
+  ccoCodigo: string;
+
+  @Expose({ name: 'CCA_EMPRESA' })
+  @Type(() => Number)
+  ccaEmpresa: number;
+
+  @Expose({ name: 'CCA_RUTA' })
+  @Type(() => Number)
+  ccaRuta: number;
+
+  @Expose({ name: 'AGE_CODIGO' })
+  @Type(() => Number)
+  ageCodigo: number;
+
+  @Expose({ name: 'AGE_NOMBRE' })
+  @Type(() => String)
+  ageNombre: string;
+
+  @Expose({ name: 'CCO_BODEGA' })
+  @Type(() => Number)
+  ccoBodega: number;
+
+  @Expose({ name: 'CCO_SERIE' })
+  @Type(() => Number)
+  ccoSerie: number;
+
+  @Expose({ name: 'AGE_CODIGO_ENT' })
+  @Type(() => Number)
+  ageCodigoEnt: number;
+
+  @Expose({ name: 'AGE_NOMBRE_ENT' })
+  @Type(() => String)
+  ageNombreEnt: string;
+
+  @Expose({ name: 'CCO_NIVEL_APROB' })
+  @Type(() => Number)
+  ccoNivelAprob: number;
+}
+
+class Cliente {
+  @Expose({ name: 'CLI_EMPRESA' })
+  @Type(() => Number)
+  cliEmpresa: number;
+
+  @Expose({ name: 'CLI_CODIGO' })
+  @Type(() => Number)
+  cliCodigo: number;
+
+  @Expose({ name: 'CLI_ID' })
+  @Type(() => String)
+  cliId: string;
+
+  @Expose({ name: 'CLI_NOMBRE' })
+  @Type(() => String)
+  cliNombre: string;
+
+  @Expose({ name: 'CLI_RUC_CEDULA' })
+  @Type(() => String)
+  CliRucCedula: string;
+
+  @Expose({ name: 'CLI_DIRECCION' })
+  @Type(() => String)
+  cliDireccion: string;
+
+  @Expose({ name: 'CLI_TELEFONO1' })
+  @Type(() => String)
+  cliTelefono1: string;
+
+  @Expose({ name: 'CLI_TELEFONO2' })
+  @Type(() => String)
+  cliTelefono2: null | string;
+
+  @Expose({ name: 'CLI_TELEFONO3' })
+  @Type(() => String)
+  cliTelefono3: null | string;
+
+  @Expose({ name: 'CLI_MAIL' })
+  @Type(() => String)
+  cliMail: null | string;
+
+  @Expose({ name: 'CLI_BLOQUEO' })
+  @Type(() => Number)
+  cliBloqueo: number | null;
+
+  @Expose({ name: 'CLI_CUPO' })
+  @Type(() => Number)
+  cliCupo: number;
+
+  @Expose({ name: 'CLI_ORDEN' })
+  @Type(() => Number)
+  cliOrden: number | null;
+
+  @Expose({ name: 'CLI_NOMBRECOM' })
+  @Type(() => String)
+  cliNombreCom: null | string;
+
+  @Expose({ name: 'CLI_LATITUD' })
+  @Type(() => Number)
+  cliLatitud: number;
+
+  @Expose({ name: 'CLI_LONGITUD' })
+  @Type(() => Number)
+  cliLongitud: number;
+
+  @Expose({ name: 'CLI_SEGMENTACION' })
+  @Type(() => Number)
+  cliSegmentacion: number | null;
+
+  @Expose({ name: 'CLI_AGENTE' })
+  @Type(() => Number)
+  cliAgente: number;
+
+  @Expose({ name: 'TCL_NOMBRE' })
+  @Type(() => String)
+  tclNombre: string;
+
+  @Expose({ name: 'CCL_NOMBRE' })
+  @Type(() => String)
+  cclNombre: string;
+
+  @Expose({ name: 'CAT_NOMBRE' })
+  @Type(() => String)
+  catNombre: string;
+
+  @Expose({ name: 'POL_NOMBRE' })
+  @Type(() => String)
+  polNombre: string;
+
+  @Expose({ name: 'CLI_LISTAPRE' })
+  @Type(() => Number)
+  cliListaPre: number;
+
+  @Expose({ name: 'PARROQUIA' })
+  @Type(() => String)
+  parroquia: string;
+
+  @Expose({ name: 'CIUDAD' })
+  @Type(() => String)
+  ciudad: string;
+
+  @Expose({ name: 'CLI_RUTA' })
+  @Type(() => Number)
+  cliRuta: number;
+
+  @Expose({ name: 'SIS_IMPUESTO_VENTA' })
+  @Type(() => Number)
+  sisImpuestoVenta: number;
+
+  @Expose({ name: 'IMP_PORCENTAJE' })
+  @Type(() => Number)
+  impPorcentaje: number;
+
+  @Expose({ name: 'CLI_SOLICITA_DATOS' })
+  @Type(() => Number)
+  cliSolicitaDatos: number | null;
+}
+
+class ListaPrecio {
+  @Expose({ name: 'DLP_CODIGO' })
+  @Type(() => Number)
+  dlpCodigo: number;
+
+  @Expose({ name: 'DLP_LISTAPRE' })
+  @Type(() => Number)
+  dlpListPre: number;
+
+  @Expose({ name: 'DLP_PRODUCTO' })
+  @Type(() => Number)
+  dlpProducto: number;
+
+  @Expose({ name: 'DLP_UMEDIDA' })
+  @Type(() => Number)
+  dlpUMedida: number;
+
+  @Expose({ name: 'UMD_ID' })
+  @Type(() => String)
+  umdId: string;
+
+  @Expose({ name: 'DLP_EMPRESA' })
+  @Type(() => Number)
+  dlpEmpresa: number;
+
+  @Expose({ name: 'DLP_ALMACEN' })
+  @Type(() => Number)
+  dlpAlmacen: number | null;
+
+  @Expose({ name: 'DLP_CATPRODUCTO' })
+  @Type(() => Number)
+  dlpCatProducto: number;
+
+  @Expose({ name: 'DLP_PRECIO' })
+  @Type(() => Number)
+  dlpPrecio: number;
+
+  @Expose({ name: 'FAC_FACTOR' })
+  @Type(() => Number)
+  facFactor: number;
+}
+class ProductoCategoria {
+  @Expose({ name: 'PCT_EMPRESA' })
+  @Type(() => Number)
+  pctEmpresa: number;
+
+  @Expose({ name: 'PCT_CODIGO' })
+  @Type(() => Number)
+  pctCodigo: number;
+
+  @Expose({ name: 'PCT_ID' })
+  @Type(() => String)
+  pctId: string;
+
+  @Expose({ name: 'PCT_NOMBRE' })
+  @Type(() => String)
+  pctNombre: string;
+}
+
+class Producto {
+  @Expose({ name: 'PRO_EMPRESA' })
+  @Type(() => Number)
+  proEmpresa: number;
+
+  @Expose({ name: 'CCO_CODIGO' })
+  @Type(() => Number)
+  ccoCodigo: number;
+
+  @Expose({ name: 'PRO_CODIGO' })
+  @Type(() => Number)
+  proCodigo: number;
+
+  @Expose({ name: 'PRO_ID' })
+  @Type(() => String)
+  proId: string;
+
+  @Expose({ name: 'PRO_NOMBRE' })
+  @Type(() => String)
+  proNombre: string;
+
+  @Expose({ name: 'UMD_ID' })
+  @Type(() => String)
+  umdId: string;
+
+  @Expose({ name: 'PRO_COMBO' })
+  @Type(() => Number)
+  proCombo: number;
+
+  @Expose({ name: 'UMD_CODIGO' })
+  @Type(() => Number)
+  umdCodigo: number;
+
+  @Expose({ name: 'CIV_CATPRODUCTO' })
+  @Type(() => Number)
+  civCatProducto: number;
+
+  @Expose({ name: 'CCO_AGENTE' })
+  @Type(() => Number)
+  ccoAgente: number;
+
+  @Expose({ name: 'UGP_USUARIO' })
+  @Type(() => Number)
+  ugpUsuario: number;
+
+  @Expose({ name: 'PRO_STOCK' })
+  @Type(() => Number)
+  proStock: number;
+
+  @Expose({ name: 'GPR_ID' })
+  @Type(() => String)
+  gprId: string;
+
+  @Expose({ name: 'PRO_PESO' })
+  @Type(() => Number)
+  proPeso: number;
+
+  @Expose({ name: 'PRO_IMPUESTO' })
+  @Type(() => Number)
+  proImpuesto: number;
+
+  @Expose({ name: 'PRO_TPRODUCTO' })
+  @Type(() => Number)
+  proTProducto: number;
+
+  @Expose({ name: 'TPR_ID1' })
+  @Type(() => String)
+  tprId1: String;
+
+  @Expose({ name: 'TPR_ID' })
+  @Type(() => String)
+  tprId: String;
+
+  @Expose({ name: 'GPR_TIPO_RENTA' })
+  @Type(() => Number)
+  gprTipoRenta: number;
+}
+
+class ProductoCombo {
+  @Expose({ name: 'PRO_CODIGO' })
+  @Type(() => Number)
+  proCodigo: number;
+
+  @Expose({ name: 'PRO_ID' })
+  @Type(() => String)
+  proId: string;
+
+  @Expose({ name: 'PRO_NOMBRE' })
+  @Type(() => String)
+  proNombre: string;
+
+  @Expose({ name: 'PRO_CODIGO_PROMO' })
+  @Type(() => Number)
+  proCodigoPromo: number;
+
+  @Expose({ name: 'PRO_ID_PROMO' })
+  @Type(() => String)
+  proIdPromo: string;
+
+  @Expose({ name: 'PRO_NOMBRE_PROMO' })
+  @Type(() => String)
+  proNombrePromo: string;
+
+  @Expose({ name: 'PRO_ENVASE' })
+  @Type(() => Number)
+  proEnvase: number | null;
+
+  @Expose({ name: 'COM_UNIDAD' })
+  @Type(() => Number)
+  comUnidad: number;
+
+  @Expose({ name: 'UMD_ID' })
+  @Type(() => String)
+  umdId: string;
+
+  @Expose({ name: 'UGP_USUARIO' })
+  @Type(() => Number)
+  ugpUsuario: number;
+
+  @Expose({ name: 'PRO_EMPRESA' })
+  @Type(() => Number)
+  proEmpresa: number;
+
+  @Expose({ name: 'PRO_IMPUESTO' })
+  @Type(() => Number)
+  proImpuesto: number;
+
+  @Expose({ name: 'GPR_CODIGO' })
+  @Type(() => Number)
+  gprCodigo: number;
+
+  @Expose({ name: 'UNIDAD_PROMO' })
+  @Type(() => String)
+  unidadPromo: string;
+
+  @Expose({ name: 'COM_CANTIDAD' })
+  @Type(() => Number)
+  comCantidad: number;
+
+  @Expose({ name: 'COM_PROMOCION' })
+  @Type(() => Number)
+  comPromocion: number;
+
+  @Expose({ name: 'PRO_TIPO_COMBO' })
+  @Type(() => Number)
+  proTipoCombo: number | null;
+}
+
+class ListaDescuento {
+  @Expose({ name: 'DLD_CODIGO' })
+  @Type(() => Number)
+  dldCodigo: number;
+
+  @Expose({ name: 'DLD_LISTAPRE' })
+  @Type(() => Number)
+  dldListPre: number;
+
+  @Expose({ name: 'DLD_PRODUCTO' })
+  @Type(() => Number)
+  dldProducto: number;
+
+  @Expose({ name: 'DLD_UMEDIDA' })
+  @Type(() => Number)
+  dldUMedida: number;
+
+  @Expose({ name: 'DLD_EMPRESA' })
+  @Type(() => Number)
+  dldEmpresa: number;
+
+  @Expose({ name: 'DLD_CLIENTE' })
+  @Type(() => Number)
+  dldCliente: number;
+
+  @Expose({ name: 'DLD_ALMACEN' })
+  @Type(() => Number)
+  dldAlmacen: number;
+
+  @Expose({ name: 'DLD_CATPRODUCTO' })
+  @Type(() => Number)
+  dldCatProducto: number;
+
+  @Expose({ name: 'DSCTO' })
+  @Type(() => Number)
+  dscto: number;
+
+  @Expose({ name: 'CLI_RUTA' })
+  @Type(() => Number)
+  cliRuta: number;
+}
+
+class TipoVisita {
+  @Expose({ name: 'TVI_CODIGO' })
+  @Type(() => Number)
+  tviCodigo: number;
+
+  @Expose({ name: 'TVI_NOMBRE' })
+  @Type(() => String)
+  tviNombre: string;
+
+  @Expose({ name: 'TVI_ACCION' })
+  @Type(() => Number)
+  tviAccion: number;
+
+  @Expose({ name: 'TVI_SOLIDAT' })
+  @Type(() => Number)
+  tviSoliDat: number | null;
+}
+
+class ListaNegraTelefono {
+  @Expose({ name: 'LNE_CODIGO' })
+  @Type(() => Number)
+  lneCodigo: number;
+
+  @Expose({ name: 'LNE_DETALLE' })
+  @Type(() => String)
+  lneDetalle: string;
+
+  @Expose({ name: 'LNE_TIPO' })
+  @Type(() => Number)
+  lneTipo: number;
+}
+class Presupuesto {
+  @Expose({ name: 'PRE_RUTA' })
+  @Type(() => String)
+  preRuta: string;
+
+  @Expose({ name: 'PRE_GPRCELYASA_ID' })
+  @Type(() => String)
+  preGprCelyasaId: string;
+
+  @Expose({ name: 'PRE_UNIDAD' })
+  @Type(() => String)
+  preUnidad: string;
+
+  @Expose({ name: 'PRE_PRESUPUESTO' })
+  @Type(() => Number)
+  prePresupuesto: number;
+}
+class PedidoSugerido {
+  @Expose({ name: 'CLI_CODIGO' })
+  @Type(() => Number)
+  cliCodigo: number;
+
+  @Expose({ name: 'CLI_ID' })
+  @Type(() => String)
+  cliId: string;
+
+  @Expose({ name: 'CLI_NOMBRE' })
+  @Type(() => String)
+  cliNombre: string;
+
+  @Expose({ name: 'CLI_AGENTE' })
+  @Type(() => Number)
+  cliAgente: number;
+
+  @Expose({ name: 'PRO_CODIGO' })
+  @Type(() => Number)
+  proCodigo: number;
+
+  @Expose({ name: 'PRO_ID' })
+  @Type(() => String)
+  proId: string;
+
+  @Expose({ name: 'PRO_NOMBRE' })
+  @Type(() => String)
+  proNombre: string;
+
+  @Expose({ name: 'CANTIDAD' })
+  @Type(() => Number)
+  cantidad: number;
+
+  @Expose({ name: 'CLI_RUTA' })
+  @Type(() => Number)
+  cliRuta: number;
+
+  @Expose({ name: 'GPR_ID' })
+  @Type(() => String)
+  gprId: string;
+}
+class AutualizacionDatosCliente {
+  @Expose({ name: 'CLI_CODIGO' })
+  @Type(() => Number)
+  cliCodigo: number;
+}
+class DatosCreaCliente {
+  @Expose({ name: 'TPC_CODIGO' })
+  @Type(() => Number)
+  tpcCodigo: number;
+
+  @Expose({ name: 'TPC_ACT_TIPOCAMPO' })
+  @Type(() => Number)
+  tpcActTipoCampo: number;
+
+  @Expose({ name: 'TPC_OPCION' })
+  @Type(() => String)
+  tpcOpcion: string;
+}
+
+class InfoCreaCliente {
+  @Expose({ name: 'CODIGO' })
+  @Type(() => Number)
+  codigo: number;
+
+  @Expose({ name: 'NOMBRE' })
+  @Type(() => String)
+  nombre: string;
+
+  @Expose({ name: 'TIPO' })
+  @Type(() => String)
+  tipo: string;
+}
+class ComboMix {
+  @Expose({ name: 'PRO_CODIGO' })
+  @Type(() => Number)
+  proCodigo: number;
+
+  @Expose({ name: 'PRO_ID' })
+  @Type(() => String)
+  proId: string;
+
+  @Expose({ name: 'PRO_NOMBRE' })
+  @Type(() => String)
+  proNombre: string;
+
+  @Expose({ name: 'RUT_CODIGO' })
+  @Type(() => Number)
+  rutCodigo: number;
+
+  @Expose({ name: 'RUT_ID' })
+  @Type(() => String)
+  rutId: string;
+
+  @Expose({ name: 'ALM_CODIGO' })
+  @Type(() => Number)
+  almCodigo: number;
+
+  @Expose({ name: 'ALM_NOMBRE' })
+  @Type(() => String)
+  almNombre: string;
+
+  @Expose({ name: 'GCM_CANAL' })
+  @Type(() => String)
+  gcmCanal: string;
+
+  @Expose({ name: 'GCM_PRINCIPAL' })
+  @Type(() => Number)
+  gcmPrincipal: number;
+
+  @Expose({ name: 'GCM_COMBO' })
+  @Type(() => Number)
+  gcmCombo: number;
+}
--- src/modules/usrcmovil/usrcmovil.service.ts
+++ src/modules/usrcmovil/usrcmovil.service.ts
@@ -1,3 +1,4 @@
+import { Cliente } from './../cliente/entities/cliente.entity';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Usrcmovil } from './entities/usrcmovil.entity';
 import { Repository } from 'typeorm';
@@ -5,6 +6,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { plainToInstance } from 'class-transformer';
 import { AuthService } from '../auth/auth.service';
 import { postSubirDataDto } from './dto/postSubirData.dto';
+import { transformDescargarDatosDto } from './dto/transformDescargarDatos.dto';
+import { log } from 'console';
 
 @Injectable()
 export class UsrcmovilService {
@@ -18,123 +21,236 @@ export class UsrcmovilService {
     try {
       const token = await this._authService.decodeToken(headers);
 
-      // ucmModulo 1 es Preventa
-      // ucmModulo 4 es Autoventa
-
-      let consulta = '';
-      if (token.ucmModulo == 1) {
-        consulta =
-          ' and C.CCO_NIVEL_APROB=0 and SYSDATE BETWEEN c.CCO_FECHA AND c.CCO_FECHAFIN';
-      } else if (token.ucmModulo == 2) {
-        consulta =
-          '  and C.CCO_NIVEL_APROB=0 and SYSDATE BETWEEN c.CCO_FECHA AND c.CCO_FECHAFIN';
-      } else if (token.ucmModulo == 4) {
-        consulta =
-          ' and C.CCO_NIVEL_APROB=1 and TRUNC(SYSDATE) BETWEEN c.CCO_FECHA AND c.CCO_FECHAFIN';
-      }
       const versionCmovil = await this._usrcmovilService.find({
         select: ['ucmNuevaVersion'],
         where: {
           ucmCodigo: token.ucmCodigo,
         },
       });
-      // const obtenerLQ = await this._usrcmovilService.query(
-      //   `select * from cm2_lq_v l where l.USR_CODIGO =${token.ucmCodigo}`,
-      // );
 
-      const obtenerLQ = await this._usrcmovilService.query(
-        `select ${token.ucmModulo} as ucm_modulo, c.* 
-        FROM CM2_LQ_TOTAL_V c where c.age_codigo =${token.ucmAgeCodigo} or c.AGE_CODIGO_ENT =${token.ucmAgeCodigo}  ` +
-          consulta,
+      // ucmModulo 1 es Preventa
+      // ucmModulo 4 es Autoventa
+      // ucmModulo 2 es Entregador
+
+      let consulta = '';
+      let obtenerLQ;
+      if (token.ucmModulo == 1) {
+        consulta =
+          ' and C.CCO_NIVEL_APROB=0 and SYSDATE BETWEEN c.CCO_FECHA AND c.CCO_FECHAFIN';
+      } else if (token.ucmModulo == 2) {
+        consulta = `
+          select ${token.ucmModulo} as ucm_modulo,
+            '' as rut_id,
+            cco.CCO_FECHA  || ' / ' || AST_GEN.NUMERO_COMPROBANTE(cco.CCO_EMPRESA, cco.CCO_CODIGO) FECHA_LIQUIDACION,
+            AST_GEN.NUMERO_COMPROBANTE(cco.CCO_EMPRESA, cco.CCO_CODIGO) LIQUIDACION,
+            cco.CCO_FECHA  || ' / ' || AST_GEN.NUMERO_COMPROBANTE(cco.CCO_EMPRESA, cco.CCO_CODIGO) LIQUIDACION_DATE,
+            cco.CCO_FECHA,
+            cco.CCO_FECHAFIN,
+            to_char(cco.cco_codigo) as CCO_CODIGO,
+            cco.CCO_EMPRESA as cca_empresa,
+            cco.cco_ruta as cca_ruta,
+            ${token.ucmAgeCodigo} as age_codigo,
+            '${token.ucmNombre}' as age_nombre,
+            cco.cco_bodega,
+            cco.cco_serie,
+            ${token.ucmAgeCodigo} as age_codigo_ent,
+            '${token.ucmNombre}' as age_nombre_ent,
+            cco.CCO_NIVEL_APROB
+            from ccomproba cco
+            WHERE cco.CCO_TIPODOC = 24
+            AND cco.CCO_ESTADO IN (2,3)
+            AND   NVL(cco.CCO_TCLIPRO,0) = 0
+            and cco.cco_agente = ${token.ucmAgeCodigo}
+            and cco.CCO_NIVEL_APROB=0 and cco.CCO_FECHAFIN = TRUNC(SYSDATE)
+            and cco.CCO_EMPRESA=2
+            `;
+        obtenerLQ = await this._usrcmovilService.query(consulta);
+      } else if (token.ucmModulo == 4) {
+        consulta =
+          ' and C.CCO_NIVEL_APROB=1 and TRUNC(SYSDATE) BETWEEN c.CCO_FECHA AND c.CCO_FECHAFIN ';
+      }
+
+      let cliente;
+      let listaPrecioClientesAux = [];
+      if (token.ucmModulo != 2) {
+        // Metodo para sacar LQ datos de Preventa y Autoventa
+        obtenerLQ = await this._usrcmovilService.query(
+          `select ${token.ucmModulo} as ucm_modulo, c.* 
+           FROM CM2_LQ_TOTAL_V c where c.age_codigo =${token.ucmAgeCodigo}` +
+            consulta,
+        );
+
+        // Metodo para sacar clientes
+        let rutas = [];
+
+        for (let index = 0; index < obtenerLQ.length; index++) {
+          const element = obtenerLQ[index];
+          rutas.push(element.CCA_RUTA);
+        }
+        cliente = await this._usrcmovilService.query(
+          ` select * from cm2_clientes_total t where t.cli_agente=${token.ucmAgeCodigo} 
+          and t.cli_ruta in (${rutas})`,
+        );
+        cliente.map((cli) => {
+          const found = listaPrecioClientesAux.find(
+            (element) => element == cli.CLI_LISTAPRE,
+          );
+          if (!found) {
+            listaPrecioClientesAux.push(cli.CLI_LISTAPRE);
+          }
+        });
+      } else {
+        // Metodo para entregador para sacar clientes
+      }
+
+      //Metodo para sacar la lista de precios
+      const listaPrecio = await this._usrcmovilService.query(
+        `
+        select * from cm2_lista_precios_v where dlp_listapre in (${listaPrecioClientesAux})
+        `,
       );
 
-      // const cliente = await this._usrcmovilService.query(
-      //   `select * from cm2_clientes t where t.AGE_CODIGO=${token.ucmAgeCodigo}`,
-      // );
+      // Metodo para sacar la categoria de productos
+      const proCategoria = await this._usrcmovilService.query(
+        `
+        select * from procategoria where pct_empresa =2
+        `,
+      );
 
-      // const producto = await this._usrcmovilService.query(
-      //   `SELECT * FROM CM2_PRODUCTOS_PRE_V P WHERE P.UGP_USUARIO=${token.ucmCodigo}`,
-      // );
+      // Metodo para sacar los productos PREVENTA
+      let producto;
+      if (token.ucmModulo == 1) {
+        producto = await this._usrcmovilService.query(
+          `
+          SELECT 
+          P.PRO_EMPRESA,
+          0 CCO_CODIGO,
+          P.PRO_CODIGO,
+          P.PRO_ID,
+          P.PRO_NOMBRE,
+          '' UMD_ID,
+          CASE WHEN ( (SELECT COALESCE(MAX(C.COM_COMPONENTE),0) FROM COMPONE C  WHERE C.COM_PRODUCTO = PRO_CODIGO)= 0) THEN
+              0
+          ELSE
+              1
+          END  PRO_COMBO,
+          0 UMD_CODIGO,
+          0 CIV_CATPRODUCTO,
+          0 CCO_AGENTE,
+          UC.UCM_CODIGO UGP_USUARIO,
+          kp_inventario.invc (P.PRO_EMPRESA,P.PRO_CODIGO,null, AL.ALM_BODEGA, null, sysdate) PRO_STOCK,
+          GC.GPR_ID,
+          P.PRO_PESO,
+          P.PRO_IMPUESTO,
+          P.PRO_TPRODUCTO,
+          ''TPR_ID1,
+          ''TPR_ID,
+          0 GPR_TIPO_RENTA
+          from PRODUCTO P 
+          JOIN GPRODUCTO GP ON(GP.GPR_CODIGO = P.PRO_GPRODUCTO AND GP.GPR_EMPRESA = P.PRO_EMPRESA)
+          JOIN USRGRUPROD UG ON (UG.UGP_EMPRESA = GP.GPR_EMPRESA AND UG.UGP_GPRODUCTO = GP.GPR_CODIGO)
+          JOIN USRCMOVIL UC ON (UG.UGP_USUARIO = UC.UCM_CODIGO AND UG.UGP_EMPRESA = UC.UCM_EMPRESA )
+          JOIN GPRCELYASA GC ON (GC.GPR_CODIGO = P.PRO_GPRCELYASA AND GC.GPR_EMPRESA = P.PRO_EMPRESA)
+          JOIN DLISTAPRE DP ON (DP.DLP_PRODUCTO = P.PRO_CODIGO AND DP.DLP_EMPRESA = P.PRO_EMPRESA)
+          JOIN ALMACEN AL ON (AL.ALM_CODIGO = UC.UCM_ALM_CODIGO AND AL.ALM_EMPRESA = UC.UCM_EMPRESA)  
+          WHERE NVL(P.PRO_INACTIVO,0)=0
+          AND NVL(GP.GPR_INACTIVO,0) = 0
+          AND NVL(P.PRO_TIPO_COMBO,0) != 2
+          AND P.PRO_EMPRESA = 2
+          AND P.PRO_GPRCELYASA IS NOT NULL
+          AND UC.UCM_VERSION>=24
+          AND UC.UCM_MODULO IS NOT NULL
+          AND UC.UCM_CODIGO= ${token.ucmCodigo}
+          AND DP.DLP_LISTAPRE IN (${listaPrecioClientesAux})
+          AND(DP.DLP_ALMACEN = UC.UCM_ALM_CODIGO OR DP.DLP_ALMACEN IS NULL)
+          AND NVL(DP.DLP_INACTIVO, 0) = 0
+          AND DP.DLP_FECHA_INI <= TRUNC(SYSDATE)
+          AND (DP.DLP_FECHA_FIN >= TRUNC(SYSDATE) OR DP.DLP_FECHA_FIN IS NULL)
+          group by p.pro_codigo, p.pro_id, p.PRO_NOMBRE, p.PRO_EMPRESA, p.PRO_IMPUESTO, p.PRO_GPRCELYASA,
+          GC.GPR_ID,UC.UCM_CODIGO,P.PRO_PESO, P.PRO_IMPUESTO, P.PRO_TPRODUCTO,AL.ALM_BODEGA
+          `,
+        );
+      }
 
-      // const productoCombo = await this._usrcmovilService.query(
-      //   `SELECT * FROM CM2_PRODUCTO_COMBO_PRE_V C WHERE C.UGP_USUARIO =${token.ucmCodigo}`,
-      // );
+      // Metodo para sacar combos de productos PREVENTA
+      const productoCombo = await this._usrcmovilService.query(
+        `SELECT * FROM CM2_PRODUCTO_COMBO_PRE_V C WHERE C.UGP_USUARIO =${token.ucmCodigo}`,
+      );
 
-      // const listaDescuentos = await this._usrcmovilService.query(
-      //   `SELECT * FROM CM2_LISTADESCUENTO`,
-      // );
+      // Metodo para sacar lista de descuentos para PREVENTA
+      const listaDescuentos = await this._usrcmovilService.query(
+        `SELECT * FROM CM2_LISTADESCUENTO where dld_listapre IN (${listaPrecioClientesAux}) `,
+      );
 
-      // const tipoVisita = await this._usrcmovilService.query(
-      //   `
-      //   select tvi_codigo, tvi_nombre, tvi_accion, tvi_solidat from v_movil_tipovisita
-      //   where tvi_inactivo = 0 and tvi_tipo = 1
-      //   `,
-      // );
+      const tipoVisita = await this._usrcmovilService.query(
+        `
+          select tvi_codigo, tvi_nombre, tvi_accion, tvi_solidat from v_movil_tipovisita
+          where tvi_inactivo = 0 and tvi_tipo = 1
+        `,
+      );
 
-      // const listNegraTelefonos = await this._usrcmovilService.query(
-      //   `select t.lne_codigo,t.lne_detalle,t.lne_tipo from lista_negra t where t.lne_empresa=2`,
-      // );
+      const listNegraTelefonos = await this._usrcmovilService.query(
+        ` select t.lne_codigo,t.lne_detalle,t.lne_tipo from lista_negra t where t.lne_empresa=2`,
+      );
 
       // const carteraTotal = await this._usrcmovilService.query(
       //   `SELECT * FROM CM2_CARTERA_V C WHERE C.AGE_CODIGO =${token.ucmAgeCodigo}`,
       // );
-      // const presupuesto = await this._usrcmovilService.query(
-      //   `select pre_ruta, pre_gprcelyasa_id, pre_unidad, pre_presupuesto from v_movil_presupuesto`,
-      // );
 
-      // const pedidoSugerido = await this._usrcmovilService.query(
-      //   `select CLI_CODIGO,CLI_ID,CLI_NOMBRE,CLI_AGENTE,PRO_CODIGO,PRO_ID,PRO_NOMBRE,CANTIDAD,CLI_RUTA,GPR_ID
-      //   from v_movil_ped_sugerido where  cli_agente = ${token.ucmAgeCodigo}`,
-      // );
+      const presupuesto = await this._usrcmovilService.query(
+        `select pre_ruta, pre_gprcelyasa_id, pre_unidad, pre_presupuesto from v_movil_presupuesto`,
+      );
 
-      // const autualizacionDatosCliente = await this._usrcmovilService.query(
-      //   `select cli_codigo from solicita_Actualizacion`,
-      // );
+      const pedidoSugerido = await this._usrcmovilService.query(
+        `select CLI_CODIGO,CLI_ID,CLI_NOMBRE,CLI_AGENTE,PRO_CODIGO,PRO_ID,PRO_NOMBRE,CANTIDAD,CLI_RUTA,GPR_ID
+        from v_movil_ped_sugerido where  cli_agente = ${token.ucmAgeCodigo}`,
+      );
 
-      // const datosCreaCliente = await this._usrcmovilService.query(
-      //   `select tpc_codigo, tpc_act_tipocampo, tpc_opcion from tipo_campo_actdat where tpc_empresa=2`,
-      // );
+      const autualizacionDatosCliente = await this._usrcmovilService.query(
+        `select cli_codigo from solicita_Actualizacion`,
+      );
 
-      // const infoCreaCliente = await this._usrcmovilService.query(
-      //   `select codigo,nombre,tipo from v_movil_cliente_datosnuevos`,
-      // );
+      const datosCreaCliente = await this._usrcmovilService.query(
+        `select tpc_codigo, tpc_act_tipocampo, tpc_opcion from tipo_campo_actdat where tpc_empresa=2`,
+      );
 
-      // const comboMix = await this._usrcmovilService.query(
-      //   `
-      //   select pro_codigo,pro_id,pro_nombre,rut_codigo,rut_id, alm_codigo,alm_nombre,gcm_canal, gcm_principal,gcm_combo
-      //   from sellerm2.v_movil_combosmix where alm_codigo = ${token.ucmAlmCodigo}
-      //   `,
-      // );
+      const infoCreaCliente = await this._usrcmovilService.query(
+        `select codigo,nombre,tipo from v_movil_cliente_datosnuevos`,
+      );
 
-      // const listaPrecio = await this._usrcmovilService.query(
-      //   `
-      //   select * from cm2_lista_precios_v
-      //   `,
-      // );
+      const comboMix = await this._usrcmovilService.query(
+        `
+        select pro_codigo,pro_id,pro_nombre,rut_codigo,rut_id, alm_codigo,alm_nombre,gcm_canal, gcm_principal,gcm_combo
+        from sellerm2.v_movil_combosmix where alm_codigo = ${token.ucmAlmCodigo}
+        `,
+      );
 
-      return {
-        cmovilVersion: versionCmovil,
+      const resultado = {
+        cmovilVersion: versionCmovil[0].ucmNuevaVersion,
         lq: obtenerLQ,
-        // cliente: cliente,
-        // producto: producto,
-        // productoCombo: productoCombo,
-        // listaDescuento: listaDescuentos,
-        // tipoVisita: tipoVisita,
-        // listaNegraTelefono: listNegraTelefonos,
+        cliente: cliente,
+        listaPrecio: listaPrecio,
+        productoCategoria: proCategoria,
+        producto: producto,
+        productoCombo: productoCombo,
+        listaDescuento: listaDescuentos,
+        tipoVisita: tipoVisita,
+        listaNegraTelefono: listNegraTelefonos,
         // carteraTotal: carteraTotal,
-        // presupuesto: presupuesto,
-        // pedidoSugerido: pedidoSugerido,
-        // autualizacionDatosCliente: autualizacionDatosCliente,
-        // datosCreaCliente: datosCreaCliente,
-        // infoCreaCliente: infoCreaCliente,
-        // comboMix: comboMix,
-        // listaPrecio: listaPrecio,
+        presupuesto: presupuesto,
+        pedidoSugerido: pedidoSugerido,
+        autualizacionDatosCliente: autualizacionDatosCliente,
+        datosCreaCliente: datosCreaCliente,
+        infoCreaCliente: infoCreaCliente,
+        comboMix: comboMix,
       };
+
+      return plainToInstance(transformDescargarDatosDto, resultado);
+      // return resultado;
     } catch (error) {
       throw new HttpException(error, HttpStatus.BAD_REQUEST);
     }
   }
-
   async subirData(postSubirDataDto: postSubirDataDto) {
     try {
       // console.log(postSubirDataDto.cmovilVersion);
